979d3c4ea43a51e1c1b582b43aed5a541c8e0420
Aswin Tom Varghese <aswin.thunchathu@gmail.com>
2018-10-18 23:17:30 +0530

    Updates

--- precache-manifest.07172b5996120a31c6c1f5c57f603902.js
+++ precache-manifest.07172b5996120a31c6c1f5c57f603902.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/DiagnalContentListing/static/js/runtime~main.277e9665.js"
   },
   {
-    "revision": "942ff1bd7ba68ff6d7ef",
-    "url": "/DiagnalContentListing/static/js/main.942ff1bd.chunk.js"
+    "revision": "24e15c2bfa612ab595dc",
+    "url": "/DiagnalContentListing/static/js/main.24e15c2b.chunk.js"
   },
   {
     "revision": "d7f41b8a6958a5fbe978",
     "url": "/DiagnalContentListing/static/js/1.d7f41b8a.chunk.js"
   },
   {
-    "revision": "942ff1bd7ba68ff6d7ef",
+    "revision": "24e15c2bfa612ab595dc",
     "url": "/DiagnalContentListing/static/css/main.0523af94.chunk.css"
   },
   {
-    "revision": "dbe9914543d859a1a122588bc390c111",
+    "revision": "f013712475def85427932010f7348691",
     "url": "/DiagnalContentListing/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/DiagnalContentListing/precache-manifest.a21a4ff087c78fd07aa523ffe6a764f2.js"
+  "/DiagnalContentListing/precache-manifest.07172b5996120a31c6c1f5c57f603902.js"
 );
 
 workbox.clientsClaim();
