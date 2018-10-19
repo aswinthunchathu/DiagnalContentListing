87e36fd0da456db427bf5b3f806ef2204711664d
Aswin Tom Varghese <aswin.thunchathu@gmail.com>
2018-10-19 16:41:15 +0530

    increased scrolling threshold for smoother scrolling

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
 export const ERROR_MESSAGE = "Oops!! something went wrong. Please try after some time."
-export const THRESHOLD = 200;
+export const THRESHOLD = 250;
 export const CACHE_CONTENT_LIST = "CACHE_CONTENT_LIST";
