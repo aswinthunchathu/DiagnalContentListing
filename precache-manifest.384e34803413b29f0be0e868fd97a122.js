e8718445851647347ce391af955352c78933b25b
Aswin Tom Varghese <aswin.thunchathu@gmail.com>
2018-10-21 11:14:12 +0530

    Updates

--- precache-manifest.384e34803413b29f0be0e868fd97a122.js
+++ precache-manifest.384e34803413b29f0be0e868fd97a122.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/DiagnalContentListing/static/js/runtime~main.277e9665.js"
   },
   {
-    "revision": "38c390c3e0d65779aa33",
-    "url": "/DiagnalContentListing/static/js/main.38c390c3.chunk.js"
+    "revision": "687f579b884d84cd91a0",
+    "url": "/DiagnalContentListing/static/js/main.687f579b.chunk.js"
   },
   {
     "revision": "2248fc499699fa31bec5",
     "url": "/DiagnalContentListing/static/js/1.2248fc49.chunk.js"
   },
   {
-    "revision": "38c390c3e0d65779aa33",
+    "revision": "687f579b884d84cd91a0",
     "url": "/DiagnalContentListing/static/css/main.8b1fb5f2.chunk.css"
   },
   {
-    "revision": "a212c9e087669125135bb9180618b27e",
+    "revision": "46877bc72f60a5d7b1c981c135af8fd4",
     "url": "/DiagnalContentListing/index.html"
   }
 ];
